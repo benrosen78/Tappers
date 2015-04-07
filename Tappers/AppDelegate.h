05f63ac35ada0e29b5ba3305a726f623a466859c
Ben Rosen <benrosen78@gmail.com>
2015-04-06 19:48:48 -0500

    Initial Commit

---
+++ Tappers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tappers
+//
+//  Created by Ben Rosen on 4/6/15.
+//  Copyright (c) 2015 Ben Rosen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
